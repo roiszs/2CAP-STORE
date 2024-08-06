1697418cd6da225abd809a030e59d45819e8584f
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-08-06 10:05:01 -0500

    Agregamos los productos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const products = [
-      { id: 1, name: 'Producto 1', price: 10.00 },
-      { id: 2, name: 'Producto 2', price: 20.00 },
-      { id: 3, name: 'Producto 3', price: 30.00 },
+      { id: 1, name: 'Producto 1', price: 10.00, image: '../images/Bass Pro Shops.png'},
+      { id: 2, name: 'Producto 2', price: 20.00, image: '../images/ct.png'},
+      { id: 3, name: 'Producto 3', price: 30.00, image: '../images/junir h.png'},
     ];
   
     const productsContainer = document.getElementById('products');
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const productElement = document.createElement('div');
       productElement.className = 'product';
       productElement.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
         <h3>${product.name}</h3>
         <p>Precio: $${product.price.toFixed(2)}</p>
         <button onclick="addToCart(${product.id})">Añadir al carrito</button>
