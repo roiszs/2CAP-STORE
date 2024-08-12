84870b04b49a531758c9ded8f2d4d0e60974a93b
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-08-12 15:34:08 -0500

    Cambios no guardados

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     //Productos destacados
     const featuredProducts = [
-      { id: 1, name: 'Bass Pro Shops', price: 10.00, image: '../images/Bass Pro Shops.png'},
-      { id: 2, name: 'Gorra CT', price: 20.00, image: '../images/ct.png'},
-      { id: 3, name: 'Gorra Junior H', price: 30.00, image: '../images/junir h.png'},
+      { id: 1, name: 'BASS PRO SHOPS', price: 800, image: '../images/Bass Pro Shops.png'},
+      { id: 2, name: 'GORRA CT', price: 600, image: '../images/ct.png'},
+      { id: 3, name: 'GORRA JUNIOR H', price: 600, image: '../images/junir h.png'},
       // Agregar mas productos aquí si es necesario
     ];
 
     //Todos los productos
     const allProducts = [
-       {id: 4, name: '2CAP', price:25.00, image: '../images/Bass Pro Shops.png' },
-       {id: 5, name: '2CAP', price:25.00, image: '../images/ct.png' },
-       {id: 6, name: '2CAP', price:25.00, image: '../images/junir h.png' },
+       {id: 4, name: 'SNOOP DOGG', price: 300, image: '../images/snoopdog.png' },
+       {id: 5, name: 'SCARFACE RED', price:450, image: '../images/scarface2.png' },
+       {id: 6, name: 'SCARFACE BLUE', price:500, image: '../images/scarface.png' },
+       {id: 7, name: 'RAIDERS', price:500, image: '../images/raiders.png' },
+       {id: 8, name: 'NY RED', price:700, image: '../images/nyroja.png' },
+       {id: 9, name: 'NY BLUE', price:700, image: '../images/nyazul.png' },
+       {id: 10, name: 'NFL ORIGINAL', price:800, image: '../images/nfl.png' },
+       {id: 11, name: 'LIVERPOOL GRAY', price:600, image: '../images/liverpoolfc.png' },
+       {id: 12, name: 'LIVERPOOL BLACK', price:600, image: '../images/liverpoolfc2.png' },
+       {id: 13, name: 'RONALDO 7', price:400, image: '../images/cristianoronaldo.png' },
+       {id: 14, name: 'SMILE', price:500, image: '../images/jokersonrisa.png' },
+       {id: 15, name: 'NY JOKER', price:350, image: '../images/joker2.png' },
+       {id: 16, name: 'JOKER ELEGANT', price:550, image: '../images/joker.png' },
        //Agregar mas productos aqui si es necesario
     ];
 
