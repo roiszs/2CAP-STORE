85e2ff09c69dae40e52835e4e1155c212d5a3696
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-08-14 19:09:00 -0500

    Diseno del perfil1

--- js/perfil.js
+++ js/perfil.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const perfilForm = document.getElementById('perfilForm');
+    const perfilNombre = document.getElementById('perfil-nombre');
+    const perfilCorreo = document.getElementById('perfil-correo');
 
     // Cargar informacion del susuario
     const nombre = localStorage.getItem('nombre') || '';
@@ -8,12 +10,19 @@ document.addEventListener('DOMContentLoaded', function () {
     perfilForm.nombre.value = nombre;
     perfilForm.correo.value = correo;
 
+    perfilNombre.textContent = nombre;
+    perfilCorreo.textContent = correo;
+
     perfilForm.addEventListener('submit', function(e) {
         e.preventDefault();
 
         //Guardar cambios
         localStorage.setItem('nombre', perfilForm.nombre.value);
         localStorage.setItem('correo', perfilForm.correo.value);
+
+        // Reflejar los cambios en la parte del perfil
+        perfilNombre.textContent = perfilForm.nombre.value;
+        perfilCorreo.textContent = perfilForm.correo.value;
 
         alert('Perfil actualizado con exito');
     });
